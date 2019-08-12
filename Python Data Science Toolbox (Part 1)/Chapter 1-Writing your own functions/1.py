ac89c2117eeff4f607b3967808d95637aa8faebe
FatiniNadhirah5 <fatininadhirahmohdnain96@gmail.com>
2019-08-12 23:19:55 +0800

    New

---
+++ Python Data Science Toolbox (Part 1)/Chapter 1-Writing your own functions/1.py
@@ -0,0 +1,32 @@
+# Write a simple function
+# In the last video, Hugo described the basics of how to define a function. You will now write your own function!
+
+# Define a function, shout(), which simply prints out a string with three exclamation marks '!!!' at the end. The code for the square() function that we wrote earlier is found below. You can use it as a pattern to define shout().
+
+# def square():
+    # new_value = 4 ** 2
+    # return new_value
+# Note that the function body is indented 4 spaces already for you. Function bodies need to be indented by a consistent number of spaces and the choice of 4 is common.
+
+# This course touches on a lot of concepts you may have forgotten, so if you ever need a quick refresher, download the Python for data science Cheat Sheet and keep it handy!
+
+# Instructions
+# 100 XP
+# Instructions
+# 100 XP
+# Complete the function header by adding the appropriate function name, shout.
+# In the function body, concatenate the string, 'congratulations' with another string, '!!!'. Assign the result to shout_word.
+# Print the value of shout_word.
+# Call the shout function.
+
+# Define the function shout
+def shout():
+    """Print a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = "congratulations" + "!!!"
+
+    # Print shout_word
+    print(shout_word)
+
+# Call shout
+shout()
---
+++ Python Data Science Toolbox (Part 2)/Chapter 1-Using iterators in PythonLand/3.py
@@ -0,0 +1,14 @@
+Iterators as function arguments
+You've been using the iter() function to get an iterator object, as well as the next() function to retrieve the values one by one from the iterator object.
+
+There are also functions that take iterators and iterables as arguments. For example, the list() and sum() functions return a list and the sum of elements, respectively.
+
+In this exercise, you will use these functions by passing an iterable from range() and then printing the results of the function calls.
+
+Instructions
+100 XP
+Instructions
+100 XP
+Create a range object that would produce the values from 10 to 20 using range(). Assign the result to values.
+Use the list() function to create a list of values from the range object values. Assign the result to values_list.
+Use the sum() function to get the sum of the values from 10 to 20 from the range object values. Assign the result to values_sum.
