bd415152eb64aa920d431209def71947b8a947bf
FatiniNadhirah5 <fatininadhirahmohdnain96@gmail.com>
2019-08-12 23:44:44 +0800

    New

---
+++ Python Data Science Toolbox (Part 2)/Chapter 1-Using iterators in PythonLand/6.py
@@ -0,0 +1,38 @@
+# Exercise
+# Exercise
+# Using * and zip to 'unzip'
+# You know how to use zip() as well as how to print out values from a zip object. Excellent!
+
+# Let's play around with zip() a little more. There is no unzip function for doing the reverse of what zip() does. We can, however, reverse what has been zipped together by using zip() with a little help from *! * unpacks an iterable such as a list or a tuple into positional arguments in a function call.
+
+# In this exercise, you will use * in a call to zip() to unpack the tuples produced by zip().
+
+# Two tuples of strings, mutants and powers have been pre-loaded.
+
+# Instructions
+# 100 XP
+# Instructions
+# 100 XP
+# Create a zip object by using zip() on mutants and powers, in that order. Assign the result to z1.
+# Print the tuples in z1 by unpacking them into positional arguments using the * operator in a print() call.
+# Because the previous print() call would have exhausted the elements in z1, recreate the zip object you defined earlier and assign the result again to z1.
+# 'Unzip' the tuples in z1 by unpacking them into positional arguments using the * operator in a zip() call. Assign the results to result1 and result2, in that order.
+# The last print() statements prints the output of comparing result1 to mutants and result2 to powers. Click Submit Answer to see if the unpacked result1 and result2 are equivalent to mutants and powers, respectively.
+# Create a zip object from mutants and powers: z1
+z1 = zip(mutants, powers)
+
+# Print the tuples in z1 by unpacking with *
+print(*z1)
+
+# Re-create a zip object from mutants and powers: z1
+z1 = zip(mutants, powers)
+
+# 'Unzip' the tuples in z1 by unpacking with * and zip(): result1, result2
+result1, result2 = zip(*z1)
+
+print(result1)
+print(result2)
+
+# Check if unpacked tuples are equivalent to original tuples
+print(result1 == mutants)
+print(result2 == powers)
