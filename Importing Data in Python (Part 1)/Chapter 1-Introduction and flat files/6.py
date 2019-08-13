7f2d11809088df8a8651fbdce5a932ebf1946dbe
FatiniNadhirah5 <fatininadhirahmohdnain96@gmail.com>
2019-08-13 23:18:35 +0800

    New

---
+++ Importing Data in Python (Part 1)/Chapter 1-Introduction and flat files/6.py
@@ -0,0 +1,16 @@
+# Working with mixed datatypes (2)
+# You have just used np.genfromtxt() to import data containing mixed datatypes. There is also another function np.recfromcsv() that behaves similarly to np.genfromtxt(), except that its default dtype is None. In this exercise, you'll practice using this to achieve the same result.
+
+# Instructions
+# 100 XP
+# Import titanic.csv using the function np.recfromcsv() and assign it to the variable, d. You'll only need to pass file to it because it has the defaults delimiter=',' and names=True in addition to dtype=None!
+# Run the remaining code to print the first three entries of the resulting array d.
+
+# Assign the filename: file
+file = 'titanic.csv'
+
+# Import file using np.recfromcsv: d
+d = np.recfromcsv(file)
+
+# Print out first three entries of d
+print(d[:3])
